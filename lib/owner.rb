15fd48b738973410139f57b9f413795f6a7439c5
github username <github email address>
2020-02-06 20:52:45 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,94 @@
 class Owner
-  # code goes here
-end
+  attr_reader :name, :species
+
+  @@all = []
+  def initialize(name=nil, species=nil)
+    @name = name
+    @species = "human"
+    @@all << self
+  end 
+
+  def say_species
+    "I am a #{@species}."
+  end 
+
+  def self.all
+    @@all 
+  end 
+
+  def self.count
+    all.count
+  end 
+
+  def self.reset_all
+    all.clear
+  end 
+
+  def cats
+    Cat.all.select do |cat|
+      cat.owner == self
+    end 
+  end 
+
+  def dogs 
+    Dog.all.select do |dog|
+      dog.owner == self
+    end 
+  end 
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end  
+
+  
+  def walk_dogs
+    Dog.all.select do |dog|
+      dog.owner == self
+    end.map do |dog|
+      dog.mood = "happy"
+    end 
+  end 
+
+  def feed_cats
+    Cat.all.select do |cat|
+      cat.owner == self
+    end.map do |cat|
+      cat.mood = "happy"
+    end 
+  end 
+
+  def sell_pets
+    Dog.all.select do |dog|
+      dog.owner == self
+    end.map do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end 
+
+    Cat.all.select do |cat|
+      cat.owner == self 
+    end.map do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end 
+  end 
+
+  def list_pets
+    dogs = Dog.all.select do |dog|
+      dog.owner == self
+    end.count
+
+    cats = Cat.all.select do |cat|
+      cat.owner == self
+    end.count
+
+    "I have #{dogs} dog(s), and #{cats} cat(s)."
+  end 
+
+
+
+end 
